d541d20ab1364f96fdc5c4b87c4339adcb75e9ce
Aravind SK <sk86.aravind@gmail.com>
2018-07-31 23:54:34 +0530

    - dataFileOps : added 'os' module calls to handle relative file paths

--- newscollector-venvp/dataFileOps.py
+++ newscollector-venvp/dataFileOps.py
@@ -6,24 +6,30 @@ from time import gmtime, strftime
 from bson import json_util
 import csv
 
+current_directory_path = os.path.abspath(os.path.dirname(__file__))
+
 def file_update_article_id(list_article_id):
-    with open('datafiles/article_ids.csv', 'a', newline='') as csvfile:
+    global current_directory_path
+    file_path = os.path.join(current_directory_path, 'datafiles/article_ids.csv')
+    with open(file_path, 'a', newline='') as csvfile:
 
         for article_id in list_article_id:
             csvfile.write(article_id)
             csvfile.write('\n')
 
 def get_article_id_from_file():
-
+    file_path = os.path.join(current_directory_path,'datafiles/article_ids.csv')
     list_article_id = list()
-    with open('datafiles/article_ids.csv', "r") as f:
+    with open(file_path, "r") as f:
         reader = csv.reader(f, delimiter="\n")
         for line in (reader):
             list_article_id.append(line[0])
     return list_article_id
 
 def write_to_data_file(entry_list,file_ts):
-    directory_path = "datafiles/staging/"+entry_list[0]['source_site']
+    global current_directory_path
+    relative_directory_path = "datafiles/staging/"+entry_list[0]['source_site']
+    directory_path = os.path.join(current_directory_path,relative_directory_path)
     if not os.path.exists(directory_path):
         os.makedirs(directory_path)
 
@@ -38,7 +44,9 @@ def write_to_data_file(entry_list,file_ts):
 
 #deprecated : replaced by get_article_id_from_file
 def get_id_list_from_data_file(source_site):
-    directory_path = 'datafiles/staging/'
+    global current_directory_path
+    relative_directory_path = 'datafiles/staging/'
+    directory_path = os.path.join(current_directory_path, relative_directory_path)
     path = directory_path+source_site
     id_list = list()
 
